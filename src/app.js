ae1d1c97461a386e226497ba2e3ce969e60f5f14
nonzwakazi.mgxaji01 <nonzwakazimgxaji@gmail.com>
2023-06-25 08:44:26 +0200

    Added search engine

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 let apiKey = "obd7f8ea0640624396b700c2ade6450t";
 let unit = "metric";
-let currentTemperature = document.querySelector("#temperature");
-let currentLocation = document.querySelector("#city-name");
+let temperature = document.querySelector("#temperature");
+let city = document.querySelector("#city-name");
 let mainWeatherIcon = document.querySelector("#main-weather-icon");
 let humidity = document.querySelector("#humidity");
 let wind = document.querySelector("#wind");
@@ -37,10 +37,10 @@ function getCurrentTemperature(response) {
   currentDateAndTime.innerHTML = formatDate(response.data.time * 1000);
 
   let roundedTemp = Math.round(response.data.temperature.current);
-  currentTemperature.innerHTML = `${roundedTemp}`;
+  temperature.innerHTML = `${roundedTemp}`;
 
   let locationName = response.data.city;
-  currentLocation.innerHTML = locationName;
+  city.innerHTML = locationName;
   document.querySelector("#country").innerHTML = response.data.country;
 
   document.querySelector("#description").innerHTML =
@@ -63,6 +63,37 @@ function getCurrentCoordinates(position) {
 
   axios.get(`${apiUrl}`).then(getCurrentTemperature);
 }
+
+function displayTemp(response) {
+  city.innerHTML = response.data.city;
+  document.querySelector("#country").innerHTML = response.data.country;
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+  let realFeel = Math.round(response.data.temperature.feels_like);
+  let humidityValue = response.data.temperature.humidity;
+  let windValue = Math.round(response.data.wind.speed);
+  feel.innerHTML = `${realFeel}°C`;
+  humidity.innerHTML = `${humidityValue}%`;
+  wind.innerHTML = `${windValue}km/h`;
+  document.querySelector("#description").innerHTML =
+    response.data.condition.description;
+  mainWeatherIcon.setAttribute("src", response.data.condition.icon_url);
+  currentDateAndTime.innerHTML = formatDate(response.data.time * 1000);
+}
+
+function displayCity(event) {
+  event.preventDefault();
+  let inputCity = document.querySelector("#input-city").value;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${inputCity}&units=${unit}&key=${apiKey}`;
+
+  axios.get(`${apiUrl}`).then(displayTemp);
+}
+let searchForm = document.querySelector("#searchcity-form");
+searchForm.addEventListener("submit", displayCity);
+
+searchForm.addEventListener("submit", function handleSubmit(event) {
+  event.preventDefault();
+  searchForm.reset();
+});
 
 function getCurrentLocation(event) {
   event.preventDefault();
